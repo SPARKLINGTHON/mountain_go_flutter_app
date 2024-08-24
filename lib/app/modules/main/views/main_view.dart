c80196f5f626ad68cc3989cd4d72005798bb9adc
Hanjun Kim <hanjunkim@Hanjunui-noteubug.local>
2024-08-25 00:09:58 +0900

    fix; main_view again

--- lib/app/modules/main/views/main_view.dart
+++ lib/app/modules/main/views/main_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:myapp/app/cores/bases/base_safe_area_view.dart';
 import 'package:myapp/app/cores/widgets/appbar.dart';
 import 'package:myapp/app/data/mountain_metadata.dart';
@@ -14,13 +15,20 @@ class MainView extends BaseSafeAreaView<MainController> {
 
   @override
   Widget body(BuildContext context) {
-    return SliverGrid(
-      delegate: SliverChildBuilderDelegate((context, index) {
-        return mountainCard(context, controller.mountainMetadataList[index]);
-      }),
-      gridDelegate:
-          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-    );
+    // return Obx(() {
+    //   return SliverGrid(
+    //     delegate: SliverChildBuilderDelegate(
+    //       (context, index) {
+    //         return mountainCard(
+    //             context, controller.mountainMetadataList[index]);
+    //       },
+    //       childCount: controller.mountainMetadataList.length
+    //     ),
+    //     gridDelegate:
+    //         const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+    //   );
+    // });
+    return Container();
   }
 
   @override
