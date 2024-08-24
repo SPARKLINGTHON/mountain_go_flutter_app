1955fb5603a14e948a63c9d30bcd03be8cbcab94
Hanjun Kim <hanjunkim@Hanjunui-noteubug.local>
2024-08-25 01:21:34 +0900

    feat; main gridview

--- lib/app/modules/main/views/main_view.dart
+++ lib/app/modules/main/views/main_view.dart
@@ -5,6 +5,8 @@ import 'package:myapp/app/cores/widgets/appbar.dart';
 import 'package:myapp/app/data/mountain_metadata.dart';
 import 'package:myapp/app/modules/main/controllers/main_controller.dart';
 
+import '../../../cores/values/app_colors.dart';
+
 class MainView extends BaseSafeAreaView<MainController> {
   MainView({super.key});
 
@@ -15,20 +17,19 @@ class MainView extends BaseSafeAreaView<MainController> {
 
   @override
   Widget body(BuildContext context) {
-    // return Obx(() {
-    //   return SliverGrid(
-    //     delegate: SliverChildBuilderDelegate(
-    //       (context, index) {
-    //         return mountainCard(
-    //             context, controller.mountainMetadataList[index]);
-    //       },
-    //       childCount: controller.mountainMetadataList.length
-    //     ),
-    //     gridDelegate:
-    //         const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-    //   );
-    // });
-    return Container();
+    return SizedBox(
+        width: Get.width,
+        height: Get.height,
+        child: GridView.builder(
+          scrollDirection: Axis.vertical,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3, childAspectRatio: 92 / 127),
+          itemBuilder: (BuildContext context, int index) {
+            return mountainCard(
+                context, controller.mountainMetadataList[index]);
+          },
+          itemCount: controller.mountainMetadataList.length,
+        ));
   }
 
   @override
@@ -37,19 +38,51 @@ class MainView extends BaseSafeAreaView<MainController> {
   }
 
   Widget mountainCard(BuildContext context, MountainMetadata data) {
-    return Stack(
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            color: controller.levelColor(data.level),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Stack(
+        children: [
+          Center(
+            child: Container(
+              decoration: BoxDecoration(
+                color: controller.levelColor(data.level),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: Container(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      for (int i = 1; i <= data.level; i++)
+                        const Icon(
+                          Icons.star,
+                          color: Colors.yellow,
+                          size: 16,
+                        ),
+                    ],
+                  ),
+                  Text(
+                    data.name,
+                    style: const TextStyle(
+                        fontSize: 12, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
           ),
-          width: 92,
-          height: 127,
-        ),
-        Positioned(
-          child: Container(),
-        )
-      ],
+        ],
+      ),
     );
   }
 }
